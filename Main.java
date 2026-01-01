5339d10e226517115f153b83497edafd0577d01b
Pravalika Sornapudi <pravalikasornapudi@gmail.com>
2026-01-01 18:25:31 +0530

    First Program

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
 
-        //This is my first Java Program
+        // This is my first Java Program
 
         /*
-            This 
-            is
-            a
-            multi-line
-            comment 
-        */
-
+         * This
+         * is
+         * a
+         * multi-line
+         * comment
+         */
 
         System.out.println("I like Biryani!");
         System.out.println("It's really delicious!");
         System.out.println("Buy me Biryani!");
+        System.out.println("");
     }
 }
