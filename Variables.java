aa67202a08436af7e7722359d65a160e596e32d7
Pravalika Sornapudi <pravalikasornapudi@gmail.com>
2026-01-01 18:39:44 +0530

    Variables

---
+++ Variables.java
@@ -0,0 +1,28 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        //declaration of variables
+        //assigning values to variables
+
+        int age = 20;
+        int year = 2026;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 8.03;
+        double temperature = 20.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        System.out.println(forSale);
+
+        
+
+    }
+}
