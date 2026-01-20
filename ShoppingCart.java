e168181992a3bcea9601c54761cb0c9be9f3cccd
Pravalika Sornapudi <pravalikasornapudi@gmail.com>
2026-01-21 05:07:18 +0530

    Files added

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
 public class ShoppingCart {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price for each?: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like to buy?: ");
+        quantity = scanner.nextInt();
+
+        total = price*quantity;
+        
+        System.out.println(total);
+    }
 }
